af8ab7e194a49c1da3595e6323220d33efa0de8f
Quentin Martel <martel.quentin@outlook.fr>
2024-05-30 10:58:38 +0200

    Updated comments correcting the code and add error handling

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,45 +1,59 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
+
 	public static void main(String args[]) {
+		
 		try {
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
-
-
+		// list of symptoms
+		ArrayList<String> symptomData = new ArrayList<String>();
 		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
+			symptomData.add(line); // add symptoms in listArrayList symptomData 
 			line = reader.readLine();	// get another symptom
 		}
 		reader.close();
+		
+		// map <symptom,number of symptom found in symptomData >
+		Map<String, Integer> symptomAndNumber = new HashMap<>();
+        for (String symptom : symptomData) { // loop for each value in the list
+            if (symptomAndNumber.containsKey(symptom)) { //if the symptom is already added to the collection we increment it by 1 else i add the symptom to the collection
+            	symptomAndNumber.put(symptom, symptomAndNumber.get(symptom) + 1);
+            } else {
+            	symptomAndNumber.put(symptom, 1);
+            }
+        }
+        
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		
+        for (Map.Entry<String, Integer> entry : symptomAndNumber.entrySet()) { // loop that reads the entire collection
+            writer.write(entry.getKey() + " : " + entry.getValue()+ "\n");
+        }
 		writer.close();
-		}catch (Exception e) {
-			// TODO: handle exception
+		
+		}catch  (FileNotFoundException e) {
+		    System.err.println("Fichier introuvable : " + e.getMessage());
+		} catch (IOException e) {
+		    System.err.println("Erreur de lecture du fichier : " + e.getMessage());
+		} catch (Exception e) {
+		    System.err.println("Erreur inattendue : " + e.getMessage());
 		}
 	}
 }
+
+	
+
+	
